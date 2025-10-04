5f4f3eae58369432ac81217fd268babff522dc2b
riii111 <ryo.ichinose@nodex.inc>
2025-10-04 16:54:37 +0900

    chore: serial port for test

--- wasabi/src/qemu.rs
+++ wasabi/src/qemu.rs
@@ -1,5 +1,5 @@
 use crate::x86::hlt;
-use crate::x86::write_io_part_u8;
+use crate::x86::write_io_port_u8;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)] // Do not rely on Rust's default representation; explicitly control it
@@ -9,7 +9,7 @@ pub enum QemuExitCode {
 }
 pub fn exit_qemu(exit_code: QemuExitCode) -> ! {
     // https://github.com/qemu/blob/master/hw/misc/debugexit.c
-    write_io_part_u8(0xf4, exit_code as u8);
+    write_io_port_u8(0xf4, exit_code as u8);
     loop {
         hlt()
     }
--- wasabi/src/x86.rs
+++ wasabi/src/x86.rs
@@ -4,6 +4,20 @@ pub fn hlt() {
     unsafe { asm!("hlt") }
 }
 
-pub fn write_io_part_u8(port: i16, data: u8) {
+pub fn busy_loop_hint() {
+    unsafe { asm!("pause") }
+}
+
+pub fn read_io_port_u8(port: u16) -> u8 {
+    let mut data: u8;
+    unsafe {
+        asm!("in al, dx",
+        out("al") data,
+            in("dx") port)
+    }
+    data
+}
+
+pub fn write_io_port_u8(port: u16, data: u8) {
     unsafe { asm!("out dx, al", in("al") data, in("dx") port) } // no OS, so need to communicate directly with the hardware
 }
