4fe65021c75e457091b7ae2650a86b537716d569
riii111 <ryo.ichinose@nodex.inc>
2025-10-25 00:46:52 +0900

    chore: draft

--- wasabi/src/x86.rs
+++ wasabi/src/x86.rs
@@ -204,3 +204,210 @@ pub unsafe fn write_gs(selector: u16) {
                 in("ax") selector)
 }
 
+#[allow(dead_code)]
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct FPUContenxt {
+    data: [u8; 512],
+}
+#[allow(dead_code)]
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct GeneralRegisterContext {
+    rax: u64,
+    rdx: u64,
+    rbx: u64,
+    rbp: u64,
+    rsi: u64,
+    rdi: u64,
+    r8: u64,
+    r9: u64,
+    r10: u64,
+    r11: u64,
+    r12: u64,
+    r13: u64,
+    r14: u64,
+    r15: u64,
+    rcx: u64,
+}
+const _: () = assert!(size_of::<GeneralRegisterContext>() == (16 - 1) * 8);
+#[allow(dead_code)]
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+struct InterruptContext {
+    rip: u64,
+    cs: u64,
+    rflags: u64,
+    rsp: u64,
+    ss: u64,
+}
+const _: () = assert!(size_of::<InterruptContext>() == 8 * 5);
+#[allow(dead_code)]
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct InterruptInfo {
+    // This struct is placed at top of the interrupt stack.
+    // Should be aligned on 16-byte boundaries to pass the
+    // alignment checks done by FXSAVE / FXRSTOR
+    fpu_context: FPUContenxt, // used by FXSAVE / FXRSTOR
+    _dummy: u64,
+    greg: GeneralRegisterContext,
+    error_code: u64,
+    ctx: InterruptContext,
+}
+const _: () = assert!(size_of::<InterruptInfo>() == (16 + 4 + 1) * 8 + 8 + 512);
+impl fmt::Debug for InterruptInfo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "
+        {{
+          rip: {:#018X}, CS: {:#06X},
+          rsp: {:#018X}, SS: {:#06X},
+          rbp: {:#018X},
+
+          rflags:      {:#018X},
+          error_code: {:#018X},
+
+          rax: {:#018X}, rcx: {:#018X},
+          rdx: {:#018X}, rbx: {:#018X},
+          rsi: {:#018X}, rdi: {:#018X},
+          r8:  {:#018X}, r9:  {:#018X},
+          r10: {:#018X}, r11: {:#018X},
+          r12: {:#018X}, r13: {:#018X},
+          r14: {:#018X}, r15: {:#018X},
+        }}",
+            self.ctx.rip,
+            self.ctx.cs,
+            self.ctx.rsp,
+            self.ctx.ss,
+            self.greg.rbp,
+            self.ctx.rflags,
+            self.error_code,
+            //
+            self.greg.rax,
+            self.greg.rcx,
+            self.greg.rdx,
+            self.greg.rbx,
+            //
+            self.greg.rsi,
+            self.greg.rdi,
+            //
+            self.greg.r8,
+            self.greg.r9,
+            self.greg.r10,
+            self.greg.r11,
+            self.greg.r12,
+            self.greg.r13,
+            self.greg.r14,
+            self.greg.r15,
+        )
+    }
+}
+
+// SDM Vol.3: 6.14.2 64-Bit Mode Stack Frame
+// In IA-32e mode, the RSP is aligned to a 16-byte boundary
+// before pushing the stack frame
+
+/// This generates interrupt_entrypointN()
+/// Generated asm will be looks like this:
+/// ```
+/// .global interrupt_entrypointN
+///    interrupt_entrypointN:
+///    push 0 // No error code
+///    push rcx // Save rcx first to reuse
+///    mov rcx, N // INT#
+///    jmp inthandler_common
+/// ```
+macro_rules! interrupt_entrypoint {
+    ($index:literal) => {
+        global_asm!(concat!(
+            ".global interrupt_entrypoint",
+            stringify!($index),
+            "\n",
+            "interrupt_entrypoint",
+            stringify!($index),
+            ":\n",
+            "push 0 // No error code\n",
+            "push rcx // Save rcx first to reuse\n",
+            "mov rcx, ",
+            stringify!($index),
+            "\n",
+            "jmp inthandler_common"
+        ));
+    };
+}
+
+interrupt_entrypoint!(3);
+interrupt_entrypoint!(6);
+interrupt_entrypoint_with_ecode!(8);
+interrupt_entrypoint_with_ecode!(13);
+interrupt_entrypoint_with_ecode!(14);
+interrupt_entrypoint!(32);
+
+extern "sysv64" {
+    fn interrupt_entrypoint3();
+    fn interrupt_entrypoint6();
+    fn interrupt_entrypoint8();
+    fn interrupt_entrypoint13();
+    fn interrupt_entrypoint14();
+    fn interrupt_entrypoint32();
+}
+
+global_asm!(
+    r#"
+.global inthandler_common
+inthandler_common:
+    // General purpose registers (except rsp and rcx)
+    push r15
+    push r14
+    push r13
+    push r12
+    push r11
+    push r10
+    push r9
+    push r8
+    push rdi
+    push rsi
+    push rbp
+    push rbx
+    push rdx
+    push rax
+    // FPU State
+    sub rsp, 512 + 8
+    fxsave64[rsp]
+    // 1st parameter: pointer to the saved CPU state
+    mov rdi, rsp
+    // Align the stack to 16-bytes boundary
+    mov rbp, rsp
+    and rsp, -16
+    // 2nd parameter: Int#
+    mov rsi, rcx
+
+    call inthandler
+
+    mov rsp, rbp
+    //
+    fxrstor64[rsp]
+    add rsp, 512 + 8
+    //
+    pop rax
+    pop rdx
+    pop rbx
+    pop rbp
+    pop rsi
+    pop rdi
+    pop r8
+    pop r9
+    pop r10
+    pop r11
+    pop r12
+    pop r13
+    pop r14
+    pop r15
+    //
+    pop rcx
+    add rsp, 8 // for Error Code
+    iretq
+"#
+);
