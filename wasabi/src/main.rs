f1303e2afdd3b8cc62ad86a57f6db891d48f819e
riii111 <ryo.ichinose@nodex.inc>
2025-11-15 19:23:07 +0900

    refactor: HPET init process

--- wasabi/src/main.rs
+++ wasabi/src/main.rs
@@ -14,9 +14,9 @@ use wasabi::graphics::draw_test_pattern;
 use wasabi::graphics::fill_rect;
 use wasabi::graphics::Bitmap;
 use wasabi::hpet::global_timestamp;
-use wasabi::hpet::set_global_hpet;
-use wasabi::hpet::Hpet;
+use wasabi::init::init_hpet;
 use wasabi::info;
+use wasabi::init::init_allocator;
 use wasabi::init::init_basic_runtime;
 use wasabi::init::init_paging;
 use wasabi::print::hexdump;
@@ -26,16 +26,11 @@ use wasabi::qemu::QemuExitCode;
 use wasabi::uefi::init_vram;
 use wasabi::uefi::locate_loaded_image_protocol;
 use wasabi::uefi::EfiHandle;
-use wasabi::uefi::EfiMemoryType;
 use wasabi::uefi::EfiSystemTable;
 use wasabi::uefi::VramTextWriter;
 use wasabi::warn;
-use wasabi::x86::flush_tlb;
 
 use wasabi::x86::init_exceptions;
-use wasabi::x86::read_cr3;
-use wasabi::x86::trigger_debug_interrupt;
-use wasabi::x86::PageAttr;
 
 #[no_mangle] // Necessary to accurately maintain the name expected by external systems
 fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
@@ -58,57 +53,13 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     let mut w = VramTextWriter::new(&mut vram);
     let acpi = efi_system_table.acpi_table().expect("ACPI table not found");
     let memory_map = init_basic_runtime(image_handle, efi_system_table);
-    let mut total_memory_pages = 0;
-    for e in memory_map.iter() {
-        if e.memory_type() != EfiMemoryType::CONVENTIONAL_MEMORY {
-            continue;
-        }
-        total_memory_pages += e.number_of_pages();
-        writeln!(w, "{e:?}").unwrap();
-    }
-    let total_memory_size_mib = total_memory_pages * 4096 / 1024 / 1024;
-    writeln!(
-        w,
-        "Total: {total_memory_pages} pages = {total_memory_size_mib} MiB"
-    )
-    .unwrap();
     writeln!(w, "Hello Non-UEFI world!").unwrap();
-    let cr3 = wasabi::x86::read_cr3();
-    println!("cr3 = {cr3:#p}");
-    let t = Some(unsafe { &*cr3 });
-    println!("{t:?}");
-    // If there is a next page, return its address
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
-    let t = t.and_then(|t| t.next_level(0));
-    println!("{t:?}");
+    init_allocator(&memory_map);
 
     let (_gdt, _idt) = init_exceptions();
-    info!("Exception initialized!");
-    trigger_debug_interrupt();
-    info!("Execution continued.");
     init_paging(&memory_map);
-    info!("Now we are using our own page tables!");
 
-    init_paging(&memory_map);
-    info!("Now we are using our own page tables!");
-
-    // Unmap page 0 to detect null ptr dereference
-    let page_table = read_cr3();
-    unsafe {
-        (*page_table)
-            .create_mapping(0, 4096, 0, PageAttr::NotPresent)
-            .expect("Failed to unmap page 0");
-    }
-    flush_tlb();
-
-    let hpet = acpi.hpet().expect("Failed to get HPET from ACPI");
-    let hpet = hpet.base_address().expect("Faild to get HPET address");
-    info!("HPET is at {hpet:#p}");
-    let hpet = Hpet::new(hpet);
-    set_global_hpet(hpet);
+    init_hpet(acpi);
     let t0 = global_timestamp();
     let task1 = Task::new(async move {
         for i in 100..=103 {
