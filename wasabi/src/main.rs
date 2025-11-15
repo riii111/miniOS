1efea0ea04039207e3ef75b9fd58fe52fa3971c2
riii111 <ryo.ichinose@nodex.inc>
2025-11-15 22:25:06 +0900

    refactor
    - BitmapTextWriter
    - init draw
    - init memory allocator
    - paging

--- wasabi/src/main.rs
+++ wasabi/src/main.rs
@@ -10,14 +10,13 @@ use wasabi::error;
 use wasabi::executor::Executor;
 use wasabi::executor::Task;
 use wasabi::executor::TimeoutFuture;
-use wasabi::graphics::draw_test_pattern;
-use wasabi::graphics::fill_rect;
-use wasabi::graphics::Bitmap;
+use wasabi::graphics::BitmapTextWriter;
 use wasabi::hpet::global_timestamp;
-use wasabi::init::init_hpet;
 use wasabi::info;
 use wasabi::init::init_allocator;
 use wasabi::init::init_basic_runtime;
+use wasabi::init::init_display;
+use wasabi::init::init_hpet;
 use wasabi::init::init_paging;
 use wasabi::print::hexdump;
 use wasabi::println;
@@ -27,9 +26,7 @@ use wasabi::uefi::init_vram;
 use wasabi::uefi::locate_loaded_image_protocol;
 use wasabi::uefi::EfiHandle;
 use wasabi::uefi::EfiSystemTable;
-use wasabi::uefi::VramTextWriter;
 use wasabi::warn;
-
 use wasabi::x86::init_exceptions;
 
 #[no_mangle] // Necessary to accurately maintain the name expected by external systems
@@ -46,11 +43,8 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     error!("error");
     hexdump(efi_system_table);
     let mut vram = init_vram(efi_system_table).expect("init_vram failed");
-    let vw = vram.width();
-    let vh = vram.height();
-    fill_rect(&mut vram, 0x000000, 0, 0, vw, vh).expect("fill_rect failed");
-    draw_test_pattern(&mut vram);
-    let mut w = VramTextWriter::new(&mut vram);
+    init_display(&mut vram);
+    let mut w = BitmapTextWriter::new(&mut vram);
     let acpi = efi_system_table.acpi_table().expect("ACPI table not found");
     let memory_map = init_basic_runtime(image_handle, efi_system_table);
     writeln!(w, "Hello Non-UEFI world!").unwrap();
